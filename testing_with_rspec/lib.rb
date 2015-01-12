c0c01322ec3dd2f019c0949fd9c79d30a6685109
Vijay Ananth <vijay.hassan@gmail.com>
2015-01-12 14:11:45 +0530

    fix minor

---
+++ testing_with_rspec/lib.rb
@@ -0,0 +1,17 @@
+class Library
+  def initialize book_list=[]
+    @books = book_list.dup
+  end
+  def add_book book
+    @books << book
+  end
+  def size
+    @books.size
+  end
+  def get_books_by_title title
+    @books.select { |book| book.title == title}
+  end
+  def get_books_by_author author
+    @books.select { |book| book.author == author}
+  end
+end
