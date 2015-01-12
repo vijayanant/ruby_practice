1ca950e1923f47f5719aab64bc415eb40ca926fd
Vijay Ananth <vijay.hassan@gmail.com>
2015-01-12 14:12:31 +0530

    simple fuzzy search

---
+++ fuzzy_search/fzerch.rb
@@ -0,0 +1,66 @@
+class FuzzySearch 
+  def initialize text, chars= ''
+    @lines = text
+    @chars = chars
+  end 
+
+  def search query=''
+    @lines.map do |line|
+      [line, score(line, query)]
+    end.select do |line, score|
+      score > 0.0
+    end.sort_by do |line, score|
+      -score
+    end.map do |line, score|
+      line
+    end
+  end 
+
+  def score choice, query
+    return 1.0 if query.length == 0
+    return 0.0 if choice.length == 0
+    query = query.downcase
+    choice = choice.downcase
+    match_length = find_matches(choice, query.each_char.to_a)
+    return 0.0 unless match_length
+
+    score = query.length.to_f / match_length.to_f
+
+    score / choice.length
+  end 
+
+  def find_matches (string, query)
+    first_char,  *rest = query
+    first_indeces = find_char(string, first_char)
+
+    first_indeces.map do |index|
+      last_index = find_end_of_match(string, rest, index)
+      last_index ? last_index - index + 1 : nil
+    end.compact.min
+  end
+
+  def find_char(string, char)
+    index = 0
+    indeces = []
+    while index 
+      index = string.index(char, index)
+      if index
+        indeces << index
+        index+=1
+      end
+    end
+      indeces
+  end
+
+  def find_end_of_match(string, chars, first)
+    last_index = first
+
+    chars.each do |this_char|
+      index = string.index(this_char, last_index + 1)
+      return nil unless index
+      last_index = index
+    end
+    last_index
+  end
+end 
+
---
+++ fuzzy_search/spec/fzerch_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../fzerch'
+require 'rspec/given'
+
+describe FuzzySearch do
+  before :all do
+    @lines = [
+      "This is a sample ipput line",
+      "this is another input line with Z"
+    ]
+      
+    @input = @lines.join("\n")
+    puts @input
+  end
+
+  describe "Set-up" do
+    context "when text and search string are given" do
+      Given(:search) {FuzzySearch.new @lines, "thp" }
+      Then { expect(search).to be_a FuzzySearch}
+    end
+  end
+
+  describe "search" do 
+    context "when no chars to match" do
+      Given(:fuzzy) { FuzzySearch.new @lines}
+      Then { expect(fuzzy.search()).to eql @lines}
+    end
+    context "when single char match" do
+      Given(:fuzzy) { FuzzySearch.new @lines}
+      Then { expect(fuzzy.search('Z').length).to eql 1}
+      Then { expect(fuzzy.search('Z')[0]).to eql @lines[1]}
+    end
+  end
+end
